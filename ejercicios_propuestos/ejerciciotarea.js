bad966487d73232d9d570ba572a1128b980bb5f4
Jesus Ernesto Paez Puentes <35340511+jesuspaez06@users.noreply.github.com>
2024-02-26 10:54:46 -0500

    Update ejerciciotarea.js

--- ejercicios_propuestos/ejerciciotarea.js
+++ ejercicios_propuestos/ejerciciotarea.js
@@ -1,8 +1,8 @@
-let valor = (numero) => {
-    let valor = numero
+let multiplotrescinco = (numero) => {
+    let valor =""
     if (numero % 3 == 0) valor = "fizz"
     if (numero % 5 == 0) valor += " buzz"
-    return valor
+    return valor|| numero
 }
 const numero = prompt("Por favor ingrese el numero ");
-alert(valor(numero))
+alert(multiplotrescinco(numero))
