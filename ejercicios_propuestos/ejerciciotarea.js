f8ff8b3f0762627bd4bda7eb7a3e2aa1d2561766
Jesus Ernesto Paez Puentes <35340511+jesuspaez06@users.noreply.github.com>
2024-02-26 10:46:47 -0500

    Update ejerciciotarea.js

--- ejercicios_propuestos/ejerciciotarea.js
+++ ejercicios_propuestos/ejerciciotarea.js
@@ -1,27 +1,8 @@
-function verificacionNum(mensaje) {//verifica si lo ingresado es un numero entero
-    let num;
-    do {
-        num = prompt(mensaje).trim();
-    } while (num === '' || isNaN(num) || !Number.isInteger(parseFloat(num)));
-    imprimir(num)
+let valor = (numero) => {
+    let valor = numero
+    if (numero % 3 == 0) valor = "fizz"
+    if (numero % 5 == 0) valor += " buzz"
+    return valor
 }
-function imprimir(num) {// imprime el resultado
-    const imprimir = comparartrescinco(parseInt(num))
-    alert(imprimir)
-}
-function comparartrescinco(num) { //compara si es multiplo de 3 y 5 al tiempo
-    if (num % 3 === 0 && num % 5 === 0) {
-        return "fizz y buzz";
-    } else { return comparartres(num) }
-}
-function comparartres(num) {//compara si es multiplo de 3
-    if (num % 3 == 0) {
-        return "fizz";
-    } else { return compararcinco(num) }
-}
-function compararcinco(num) {//compara si es multiplo de 5
-    if (num % 5 == 0) { return "buzz" }
-    return num //si no es multiplo de ambos casos solo retorna el numero
-}
-
-let num = verificacionNum("ingrese el numero ");//recibe la variable y la manda a la funcion de verificar
+const numero = prompt("Por favor ingrese el numero ");
+alert(valor(numero))
