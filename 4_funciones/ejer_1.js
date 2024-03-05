8f8ffb40b540ef973cc97ae261b72c73c2cdb419
Jesus Ernesto Paez Puentes <35340511+jesuspaez06@users.noreply.github.com>
2024-03-05 17:50:04 -0500

    Update ejer_1.js

--- 4_funciones/ejer_1.js
+++ 4_funciones/ejer_1.js
@@ -1,4 +1,4 @@
 const parflecha = (num) => num%2==0 ?`es par`:`es impar` ;
-do {num = prompt("Por favor ingrese el numero ");
-} while (num.trim() === '' || isNaN(num.trim()) || +num.trim() == 0);
+do {num = prompt("Por favor ingrese el numero ").trim();
+} while (num === '' || isNaN(num) || +num == 0);
 console.log(parflecha(num));
