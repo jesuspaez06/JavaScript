b11c13a8826e70574e149d9a0fa1ad99f98c8627
Jesus Ernesto Paez Puentes <35340511+jesuspaez06@users.noreply.github.com>
2024-03-05 19:08:31 -0500

    Delete 6_arrays directory

--- 6_arrays/ejer_6.js
+++
@@ -1,3 +0,0 @@
-let numerostring ="1,2,3,4,5"
-let array=numerostring.split(",").map(numero => parseFloat(numero));
-console.log (array)
--- 6_arrays/ejer_7.js
+++
@@ -1,3 +0,0 @@
-let array = [1,2,3,4,5]
-let arraymulti=array.map(numero => numero*2);
-console.log(arraymulti)
